8e321f72305aea114665af7fd4ab4b5294c1944d
joohee lee <jooheelee@jooheeui-MacBook-Pro.local>
2018-12-27 00:21:39 +0900

    tab component

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Jooui 
+# Jooui -  Ui pattern 
+
+# UI pattern 정의 
+1. 필요한 컴포넌트를 정의한다.
+2. 기능별로 컴포넌트(component) 를  구조 및 디자인을 ( Html: markup / CSS)한다. 
+3. ui.js
+
+## UI pattern 명세서 
+-  Ui markup / css Guide
+-  Ui Script Guide
+
+### Tab
+1. Tab - simple 
+- Ui tab simple markup / css
+- Ui tab show / hide function add
+
+
+
+
+
+
---
+++ js/ui.js
@@ -0,0 +1,175 @@
+"use strict";
+
+// object literal desigin
+
+/* used Attribute  
+ * data-*
+ * @param [String] el (element or component.. )
+ * @param [String] key (data-*)
+ * @param [Number] val (value , 0 , 1, 2 ....)
+ * @method : getData, setData, removeData  
+*/
+
+var dataset = {
+
+	getData : function( el, key ){
+		return el.getAttribute(key);
+	},
+
+	setData : function( el, key, val ){
+		el.setAttribute( key, val );
+	},
+
+	removeData : function( el, key ){
+		el.removeAttibute( key );
+	}
+
+};
+
+/* used ClassName
+	//class 유효 체크 
+	//addClass
+	//removeClass
+*/
+
+
+/* cc
+ * @param [Number] idx / current value 
+ * @param [Number] len / item length  
+ * @method nextIdx / prevIdx 
+*/
+var  cc = {
+
+	nextIdx : function( idx, len ){
+
+		idx++;
+
+		if ( idx === len){
+			idx = 0;
+		}
+
+		return idx;
+	},
+
+	prevIdx : function( idx, len ){
+
+		idx--;
+
+		if ( idx < 0 ){
+			idx = len - 1;
+		}
+
+		return idx;
+	}
+
+
+};
+
+/* Tab - simple 
+ * data-current / data-idx
+ * @param [Element] cpnt : component name ID 
+ * @param [function] init : bindingId
+ * @param [function] _setCurrent : setting data-current
+*/
+var uiTab = {
+
+	init : function( cnpt, el ){
+		var elCnpt = document.getElementById(cnpt);
+		var idx = dataset.getData(el, 'data-idx');
+		this._setCurrent( elCnpt, idx);
+	},
+
+	_setCurrent : function( cnpt, val ){
+		dataset.setData( cnpt, 'data-current' , val);
+	}
+
+};
+
+/* Tab - multi function 
+ * data-current 
+ * @param [Number] leng, item's length
+ * @param [Number] time, auto play interval time
+ * @param [Number] current, 
+ * @param [] interval, 
+ * @method init / setCurrent / prev / next  / prev / stop
+*/
+
+var uiTabModule = {
+
+	//property:value
+	elCnpt: '',
+	elItem: '',
+	current: 0,
+	leng: 0,
+	time: 2000,
+	interval: '',
+
+
+	init: function( cnpt ){
+		this.elCnpt = document.getElementById(cnpt);
+		this.elItem = this.elCnpt.querySelectorAll('.tab-item');
+		this.current = dataset.getData(this.elCnpt, 'data-current');
+		this.leng = this.elItem.length;
+	},
+
+	_setCurrent : function( cnpt, val ){
+		dataset.setData( cnpt, 'data-current', val);
+	},
+
+	tabCurrent: function( cnpt, el ){
+
+		//this.stop(); // play 를 우선 stop 하고 
+		this.init(cnpt);
+		var idx = dataset.getData(el, 'data-idx');
+		this._setCurrent( this.elCnpt, idx);
+		//this.play(cnpt); // 다시 play
+
+	},
+
+	next : function( cnpt ){
+		this.init(cnpt);
+		this.current = cc.nextIdx( this.current, this.leng);
+		this._setCurrent(this.elCnpt, this.current);
+	},
+
+	prev : function( cnpt ){
+		this.init(cnpt);
+		this.current = cc.prevIdx( this.current, this.leng);
+		this._setCurrent(this.elCnpt, this.current);
+	},
+
+	play : function( cnpt ){
+		console.log('play');
+
+		this.init(cnpt)
+		var self = this;
+		this.interval = setInterval(function(){ self.next(cnpt);}, this.time);
+		dataset.setData( this.elCnpt, 'data-control', 'play');
+	},
+
+	stop : function(){
+		console.log('stop');
+		
+		clearInterval(this.interval);
+		dataset.setData( this.elCnpt, 'data-control', 'pause');
+	}
+
+};
+
+
+/* Toggle 
+ * data - expended
+
+*/
+var uiToggle ={
+
+
+};
+
+/* accordion 
+
+*/
+
+/* img - rolling 
+*/
+
